e9053246b9953edaa4037b4109ba27e8a93b4938
ArsalJan34 <arsaljaan532@gmail.com>
2026-01-19 17:34:02 +0500

    Changes made

--- views.py
+++ views.py
@@ -5,4 +5,7 @@ views = Blueprint(__name__, "views")
 @views.route("/")
 def home():
   return render_template("index.html")
-
+# here we can also pass names and access them in html
+  # return render_template("index.html", name="Joe")
+  # and we access them in html by using double curly braces {{}}
+#
